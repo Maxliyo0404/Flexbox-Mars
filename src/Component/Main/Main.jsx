01022cbace529149580d5db31232393792c5c26c
Mahliyo <gasandigital13@gmail.com>
2026-05-13 23:01:13 +0500

    hero start css responsive fnish

---
+++ src/Component/Main/Main.jsx
@@ -0,0 +1,14 @@
+import { useTranslation } from "react-i18next";
+import "./Main.css";
+import React from 'react'
+
+function Main() {
+    const {t, i18n} = useTranslation();
+  return (
+   <>
+   
+   </>
+  )
+}
+
+export default Main
