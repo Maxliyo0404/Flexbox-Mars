7b4cb93492304057ebb2b285b911cb77462eb631
Mahliyo <gasandigital13@gmail.com>
2026-05-13 23:23:15 +0500

    main start container list rasm

--- src/Component/Main/Main.jsx
+++ src/Component/Main/Main.jsx
@@ -1,6 +1,9 @@
 import { useTranslation } from "react-i18next";
 import "./Main.css";
 import React from 'react'
+import main_1 from "./image/main_1.svg";
+import main_2 from "./image/main_2.svg";
+
 
 function Main() {
     const {t, i18n} = useTranslation();
@@ -11,33 +14,33 @@ function Main() {
             <div className="main-wrapper">
                 <ul  className="main-list">
                     <li className="main-item">
-                        <img src={} alt="main" />
-                        <h2 className="main-title">{t("main.main-title")}</h2>
+                        <img src={main_1} alt="main" />
+                        <h2 className="main-title">{t("main.main-title1")}</h2>
+                        <p className="main-text">{t("main.main-text")}</p>
+                    </li>
+                    <li className="main-item">
+                        <img src={main_2} alt="main" />
+                        <h2 className="main-title">{t("main.main-title2")}</h2>
                         <p className="main-text">{t("main.main-text")}</p>
                     </li>
                     <li className="main-item">
                         <img src={} alt="main" />
-                        <h2 className="main-title">{t("main.main-title")}</h2>
+                        <h2 className="main-title">{t("main.main-title3")}</h2>
                         <p className="main-text">{t("main.main-text")}</p>
                     </li>
                     <li className="main-item">
                         <img src={} alt="main" />
-                        <h2 className="main-title">{t("main.main-title")}</h2>
+                        <h2 className="main-title">{t("main.main-title1")}</h2>
                         <p className="main-text">{t("main.main-text")}</p>
                     </li>
                     <li className="main-item">
                         <img src={} alt="main" />
-                        <h2 className="main-title">{t("main.main-title")}</h2>
+                        <h2 className="main-title">{t("main.main-title2")}</h2>
                         <p className="main-text">{t("main.main-text")}</p>
                     </li>
                     <li className="main-item">
                         <img src={} alt="main" />
-                        <h2 className="main-title">{t("main.main-title")}</h2>
-                        <p className="main-text">{t("main.main-text")}</p>
-                    </li>
-                    <li className="main-item">
-                        <img src={} alt="main" />
-                        <h2 className="main-title">{t("main.main-title")}</h2>
+                        <h2 className="main-title">{t("main.main-title3")}</h2>
                         <p className="main-text">{t("main.main-text")}</p>
                     </li>
                 </ul>
